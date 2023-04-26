7f58152e6163210adf0b5d3cb69fec2a1cadcc9d
Pratik1603 <pratikgupta1603@gmail.com>
2023-04-26 14:53:54 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import Home from "./pages/home/Home"
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+
+
+
+function App() {
+
+  
+  return (
+    
+    <div >
+      <BrowserRouter>
+        <Routes>
+         
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+    
+    
+         
+  );
+}
+
+export default App;
