fea433468f20a45406932bff4c60a149cca676bd
Devvrat Gupta <devvrat.gupta11@gmail.com>
2023-09-05 22:39:22 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,25 @@
-import Home from "./pages/home/Home"
 import {
   BrowserRouter,
   Route,
   Routes
 } from "react-router-dom";
-
-
+import Home from "./Components/Pages/HomePage/Home";
+import Navbar from "./Components/Pages/Navbars/Navbar";
+import Movies from "./Components/Pages/MovieCart/MovieCart";
+import Profile from "./Components/Pages/ProfilePage/Profile";
 
 function App() {
-
-  
   return (
-    
     <div >
-      <BrowserRouter>
+      <Navbar/>
+     
         <Routes>
-         
+        <Route path="/" element={<Home/>}/>
+        <Route path="/movies" element={<Movies/>}/>
+        <Route path="/profile" element={<Profile/>}/>
         </Routes>
-      </BrowserRouter>
-
+     
     </div>
-    
-    
-         
   );
 }
 
